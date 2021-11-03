e805b36c7ff236b197db0e3bcd9dae9a81bb8397
Whtneyas <90154152+Whtneyas@users.noreply.github.com>
2021-11-03 14:17:38 +0100

    Eindversie

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 console.log("9292");
 
-/* ik heb de knop van een van de knop gelecteerd */
+/* ik heb de knop van een van de knoppen gelecteerd */
 var articleButton = document.querySelector(".button");
-var text = document.querySelector("div");  /*de text die ik voorschijnen ook geslecteerd*/
+var text = document.querySelector("div");  
 
 function dropDown(){
     text.classList.toggle("meertext");
